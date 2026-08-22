ecd94026dec570eefc9715197b753ab218032070
aoodycom-cmyk <aoody.com@hotmail.com>
2026-08-22 13:51:22 +0300

    Sync Supabase client config to public deploy

--- public/backend-config.js
+++ public/backend-config.js
@@ -1,1 +1,3 @@
 window.FRANKLIN_BACKEND_URL = "";
+window.FRANKLIN_SUPABASE_URL = "https://djptdxocpslfowshbtir.supabase.co";
+window.FRANKLIN_SUPABASE_PUBLISHABLE_KEY = "sb_publishable_5BLb5fpkzf4_7qgPb5smLw_XWMVSuPE";
